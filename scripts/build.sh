8dabae4eb6665d0eb759067011383b0f75b4dc1f
rick <rick@ricks-MacBook-Air.local>
2021-11-18 18:01:17 -0500

    m

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,7 @@
-go build && ( systemctl stop vpntech-guard ; cp guard /opt/vpntech-binaries/x86_64 && systemctl start vpntech-guard ) && systemctl status --no-pager vpntech-guard && journalctl -fu vpntech-guard
+go build
+if command -v systemctl >/dev/null 2>&1; then
+( 
+  systemctl stop vpntech-guard ; cp guard /opt/vpntech-binaries/x86_64 && systemctl start vpntech-guard 
+) && systemctl status --no-pager vpntech-guard && journalctl -fu vpntech-guard
+
+fi
